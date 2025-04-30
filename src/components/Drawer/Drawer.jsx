99b0d49fe7d79cbd25a8864c949577322936b6cd
DESKTOP-1LJ402J\rlarnjsgn <arsky12@naver.com>
2025-04-30 16:38:50 +0900

    드로워 수정

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -9,6 +9,7 @@ import { People } from "../People/People";
 import { PopUp } from "../PopUp/PopUp";
 
 import { drawerStyle } from "./DrawerStyle";
+import { useAuthStore } from "../../store/authStore";
 
 export const Drawer = ({
     isOpen,
@@ -22,6 +23,8 @@ export const Drawer = ({
         setPopUpType(type)
     }
 
+    const { user } = useAuthStore();
+
     // const getRoomList = useCallback(() => {
     //     sock
     // })
@@ -33,66 +36,76 @@ export const Drawer = ({
             </TouchableWithoutFeedback>
 
             <View style={drawerStyle.container}>
-                <View style={drawerStyle.search}>
-                    <Input
-                        placeholder={"Search..."}
+                {
+                    user
+                    ?
+                    <>
+                        <View style={drawerStyle.search}>
+                        <Input
+                            placeholder={"Search..."}
 
-                        icon={require("../../assets/images/Search.png")}
+                            icon={require("../../assets/images/Search.png")}
 
-                        borderColor={Colors.sub2}
-                        placeholderColor={Colors.sub2}
-                    />
-                </View>
-                <View>
-                    <View style={drawerStyle.title}>
-                        <Text style={drawerStyle.name}>워크 스페이스</Text>
-                        <TouchableOpacity onPress={() => openPopUp('workspace')}>
-                            <Image 
-                                style={drawerStyle.image} 
-                                source={require("../../assets/images/Plus.png")}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <Channel 
-                        name={'IMPL'}
-
-                        count={5}
-                    />
-                    <Channel 
-                        name={'IMPL 대학원생'}
-
-                        count={12}
-                    />
-                </View>
-                <View>
-                    <View style={drawerStyle.title}>
-                        <View style={drawerStyle.members}>
-                            <Image 
-                                style={[drawerStyle.image, { marginRight: 10 }]} 
-                                source={require("../../assets/images/Members.png")} 
-                            />
-                            <Text style={drawerStyle.name}>구성원</Text>
+                            borderColor={Colors.sub2}
+                            placeholderColor={Colors.sub2}
+                        />
                         </View>
-                        <TouchableOpacity onPress={() => openPopUp('member')}>
-                            <Image 
-                                style={drawerStyle.image} 
-                                source={require("../../assets/images/Plus.png")} 
+                        <View>
+                            <View style={drawerStyle.title}>
+                                <Text style={drawerStyle.name}>워크 스페이스</Text>
+                                <TouchableOpacity onPress={() => openPopUp('workspace')}>
+                                    <Image 
+                                        style={drawerStyle.image} 
+                                        source={require("../../assets/images/Plus.png")}
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                            <Channel 
+                                name={'IMPL'}
+
+                                count={5}
                             />
-                        </TouchableOpacity>
+                            <Channel 
+                                name={'IMPL 대학원생'}
+
+                                count={12}
+                            />
+                        </View>
+                        <View>
+                            <View style={drawerStyle.title}>
+                                <View style={drawerStyle.members}>
+                                    <Image 
+                                        style={[drawerStyle.image, { marginRight: 10 }]} 
+                                        source={require("../../assets/images/Members.png")} 
+                                    />
+                                    <Text style={drawerStyle.name}>구성원</Text>
+                                </View>
+                                <TouchableOpacity onPress={() => openPopUp('member')}>
+                                    <Image 
+                                        style={drawerStyle.image} 
+                                        source={require("../../assets/images/Plus.png")} 
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                            <People
+                                name = {'장승훈'}
+                                status={'online'}
+                            />
+                            <People
+                                name = {'이영희'}
+                                status={'offline'}
+                            />
+                            <People
+                                name = {'앙드레킴'}
+                                status={'away'}
+                            />
+                        </View>
+                    </>
+                    :
+                    <View>
+                        <Text>로그인이 필요합니다.</Text>
                     </View>
-                    <People
-                        name = {'장승훈'}
-                        status={'online'}
-                    />
-                    <People
-                        name = {'이영희'}
-                        status={'offline'}
-                    />
-                    <People
-                        name = {'앙드레킴'}
-                        status={'away'}
-                    />
-                </View>
+                }
             </View>
             <PopUp
                 popOpen={popOpen}
